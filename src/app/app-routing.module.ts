e4636584502f09fc393eff85b16cdca0c089ee3d
Sameer Khanal <khanal.sam91@gmail.com>
2018-09-10 17:00:22 +1200

    setup required working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,13 @@ import {FeatureComponent} from './feature/feature.component';
 import {AboutUsComponent} from './about-us/about-us.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {HomeComponent} from './home/home.component';
-import {AuthGuard} from './_guards/auth.guard';
 import {SettingsComponent} from './settings/settings.component';
 
 const appRoutes: Routes = [
   {path: ':userShortCode/home', component: HomeComponent},
   {path: ':userShortCode/setup', component: SettingsComponent},
-  {path: 'features', component: FeatureComponent},
-  {path: 'aboutus', component: AboutUsComponent},
+  {path: ':userShortCode/features', component: FeatureComponent},
+  {path: ':userShortCode/aboutus', component: AboutUsComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AppService} from '../_services/app.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public appService: AppService) { }
 
   ngOnInit() {
   }
